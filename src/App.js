785e30e29ae1f4cb3570717d2520d0865a1d6890
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-04-15 23:54:53 +0600

    make home food section

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import Login from "./Pages/Login/Login";
 import Navbar from "./Pages/Shared/Navbar/Navbar";
 import SignUp from "./Pages/SignUp/SignUp";
 import Footer from "./Pages/Shared/Footer/Footer";
+import Dinner from "./Pages/Home/Dinner/Dinner";
+import Breakfast from "./Pages/Home/Breakfast/Breakfast";
+import Lunch from "./Pages/Home/Lunch/Lunch";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}>
+          <Route path="breakfast" element={<Breakfast />} />
+          <Route path="lunch" element={<Lunch />} />
+          <Route path="dinner" element={<Dinner />} />
+        </Route>
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
